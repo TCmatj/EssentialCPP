2d5c61cd1feadcf10eda284369b113831ef7b323
TCmatj <2216685752@qq.com>
2022-04-26 20:42:36 +0800

    chapter3

--- chapter3/3_05.cpp
+++ chapter3/3_05.cpp
@@ -14,9 +14,22 @@ bool is_elem(vector<int> &vec, int elem){
     return binary_search(vec.begin(), vec.end(), elem);
 }
 
+bool grow_vec(vector<int>& vec, int elem){
+    while(vec.back() < elem){
+        vec.push_back(vec.back() + vec[vec.size() - 2]);
+    }
+    if(vec.back() == elem)
+        return true;
+    else
+        return false;
+}
+
 int main(){
-    int arr[6] = {1, 2, 5, 8, 11, 12};
+    int arr[6] = {1, 1, 2, 3, 5, 8};
     vector<int> svec(arr, arr + 6);
-    cout << is_elem(svec,14) << endl;
+    cout << "查询元素是否属于斐波那契数列中的元素\n请输入元素：";
+    int num;
+    cin >> num;
+    cout << is_elem(svec,num) << endl;
     return 0;
 }
